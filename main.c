0ff55b4e3d91e31f447a97e54a4879eeb8734206
Ray Xu <rxu50@stuy.edu>
2024-12-02 17:33:24 -0500

    Removed parse.h and incorporated into main.c directly

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 #include <string.h>
 #include <errno.h>
 
+void parse_args( char * line, char ** arg_ary ) {
+    char * token = NULL;
+    int counter = 0;
+    while ((token = strsep(&line, " "))) {
+        arg_ary[counter] = token;
+        counter++;
+    } 
+    arg_ary[counter] = NULL;
+}
+
 int main(){
     while (1) {
         char cwd[100];
